57bb0d1594c1723a065eee4f71abf5574bcc2569
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-10-10 19:57:16 +0100

    users, games, players set up

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,38 @@
+class GamesController < ApplicationController
+  before_action :authenticate_user!, :except => [:index, :show]
+
+  def new
+  end
+
+  def index
+    @games = Game.all
+  end
+
+  def create
+    @game = Game.new(game_params)
+    @game.save
+    @game.players.new(user_id: current_user.id, game_id: @game.id)
+    @game.save
+    redirect_to @game
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  def edit
+    @game = Game.find(params[:id])
+  end
+
+  def update
+    @game = Game.find(params[:id])
+    @game.players.new(user_id: current_user.id, game_id: @game.id)
+    @game.save
+    redirect_to "/games/#{@game.id}"
+  end
+
+  private
+  def game_params
+    params.require(:game).permit(:title, :time)
+  end
+end
---
+++ db/migrate/20161010154722_add_fields_to_tables.rb
@@ -0,0 +1,6 @@
+class AddFieldsToTables < ActiveRecord::Migration
+  def change
+    #add_column :players, :user_id, :integer
+    #add_column :players, :game_id, :integer
+  end
+end

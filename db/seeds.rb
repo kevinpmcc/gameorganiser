1458e996a10a5318b9ad201f8e2c058913c30f4d
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2017-01-13 15:37:28 +0000

    added game to sheets

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,6 +40,7 @@ Boardgame.create! ({ title: 'Forbidden Desert' })
 Boardgame.create! ({ title: 'Coup: The Resistance' })
 Boardgame.create! ({ title: 'The Ravens of Thri Sahashri' })
 Boardgame.create! ({ title: 'Discworld: Ankh-Morpork' })
+Boardgame.create! ({ title: 'Trivial Pursuit' })
 
 available_times = ['14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00']
 
